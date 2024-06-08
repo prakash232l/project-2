7ce79b0cf49fa51a9e35b86356ad6963f9d2c6c3
prakash232l <gp827041@gmail.com>
2024-06-08 23:26:34 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "prakashraj"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "prakash"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PKJ"
 )
 
 var ID ids.ID
